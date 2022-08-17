88d46153b271e518dfe7cc1b6c485b1d604be1d8
enesrobloxta <91794198+enesrobloxta@users.noreply.github.com>
2022-08-17 13:26:40 -0700

    dumb update

--- hesaplayıcı.cpp
+++ hesaplayıcı.cpp
@@ -1,144 +1,128 @@
 #include <iostream>
-#include <string>
-#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <limits>
 
 using namespace std;
 
-int hesapla(){
+typedef numeric_limits< double > dbl;
 
-    int x[1000000];
-    double sum;
-    char ch;
-    int i=2;
-    cin >> x[1];
-    x[0]=x[1];
-    cin.ignore();
-    cin >> ch;
+double math( double a,double tmp,char x[]){
 
-    x:
-    cin.ignore();
-    switch(ch){
-        case 43://+
-        cin >> x[i];
-        cin.ignore();
-        cin >> ch;
-        x[0]+=x[i];
-        if(ch == 3){    
-            return x[0];
-            cout << x[0] << endl;
-        }
-        else
-            i++;
-            goto x;
-        case 45://-
-        cin >> x[i];
+    if(!strcmp(x,"+")){
+        if(tmp == -1234567.1234567)
+            cin >> tmp;
+        return a+tmp;
+    
+    }else if(!strcmp(x,"-")){
+        if(tmp == -1234567.1234567)
+            cin >> tmp;
+        return a-tmp;
+         
+        
+    }else if(!strcmp(x,"*")){
+        if(tmp == -1234567.1234567)
+            cin >> tmp;
+         return a*tmp;
+        
+    }else if(!strcmp(x,"/")){
+        if(tmp == -1234567.1234567)
+            cin >> tmp;
+         return a/tmp;
+        
+    }else if(!strcmp(x,"q")){
+         return sqrt(a);
+        
+    }else if(!strcmp(x,"p")){
+        if(tmp == -1234567.1234567)
+            cin >> tmp;
+        return pow(a,tmp);
 
-        cin.ignore();
-        cin >> ch;
-        x[0]-=x[i];        
-        if(ch == 3){    
+    }else if(!strcmp(x,"a")){
+         return abs(a);
 
-            return x[0];
-            cout << x[0] << endl;
-        }
-        else
-            i++;
-            goto x;
-        case 42://*
-        cin >> x[i];
-        cin.ignore();
-        cin >> ch;
-        x[0]*=x[i];
-        if(ch == 3){    
-            
-            return x[0];
-            cout << x[0] << endl;
-        } 
-        else
-            i++;
-            goto x;
-        case 47:// /
-        cin >> x[i];
-        cin.ignore();
-        cin >> ch;
-        x[0]/=x[i];
-        if(ch == 3){    
-          
-            return x[0];
-            cout << x[0] << endl;
-        }
-        else
-            i++;
-            goto x;
+    }else if(!strcmp(x,"r")){
+        return round(a);
+
+    }else {
+        return 0;
     }
+    
 }
+    int main(){
 
-int main(){
+    cout.precision(8);
+    double sum=0,num1=0,num2=0;
+    char ch[3],ch2[3];
+    cout << "Lutfen her karakterden sonra entr a basiniz cikmak icin end yaziziniz" <<endl;
+    cout << "KOMUTLAR:" << endl <<
+    "carpma(*) bolme(/) toplama(+) cikarma(-) "<< endl << 
+    "kare kok alma(q) ust alma(p) mutlak deger(a) yuvarla(r)"<< endl;
 
 
-    int x[1000000];
-    double sum;
-    char ch;
-    int i=2;
-    cin >> x[1];
-    x[0]=x[1];
-    cin.ignore();
+    cout << "Num1:" << endl;
+    cin >> num1;
+
+    
+
+    do{
+        cout << "islemi giriniz(islem yapmicaksaniz a ya basiniz) "<<endl;
+        cin >> ch;
+        if(!strcmp(ch,"end"))
+            exit(0);
+        // check if the ch is equals to enter
+        if(!strcmp(ch,"a") || !strcmp(ch,"A"))
+            break;
+
+        num1=math(num1,-1234567.1234567,ch);
+        cout << fixed <<num1 << endl;
+        
+    }while(1);
+
+    do{
+    
+    cout << "islemi giriniz" << endl;
+    cin >> ch2;
+    if(!strcmp(ch2,"end"))
+        exit(0);
+
+    if(!strcmp(ch2,"a") ||!strcmp(ch2,"q")||!strcmp(ch2,"r")){
+        sum=math(num1,0,ch2);
+        cout << fixed << sum << endl;
+        goto s;
+    }
+
+
+    cout << "Num2:" << endl;
+    cin >> num2;
+    
+    do{
+        cout << "islemi giriniz(islem yapmicaksaniz a ya basiniz) "<<endl;
+        cin >> ch;
+        if(!strcmp(ch,"end"))
+            exit(0);
+        // check if the ch is equals to enter
+        if(!strcmp(ch,"a") || !strcmp(ch,"A"))
+            break;
+
+        num2=math(num2,-1234567.1234567,ch);
+        cout << fixed <<num2 << endl;
+        
+    }while(1);
+
+    sum=math(num1,num2,ch2);
+    cout << fixed << sum <<  endl <<
+    "Devam edecekseniz c ye ,devam etmiceksiniz end yaziniz" << endl;
+    num1=sum;
+    s:
+
     cin >> ch;
+    
+    if(!strcmp(ch,"end")||!strcmp(ch,"END"))
+        break;
+    
+    }while(1);
 
-    x:
-    cin.ignore();
-    switch((int)ch){
-        case 43://+
-        cin >> x[i];
-        cin.ignore();
-        cin >> ch;
-        x[0]+=x[i];
-        if((int)ch == 3){    
-            return x[0];
-            cout << x[0] << endl;
-        }
-        else
-            i++;
-            goto x;
-        case 45://-
-        cin >> x[i];
+    exit(0);
 
-        cin.ignore();
-        cin >> ch;
-        x[0]-=x[i];        
-        if((int)ch == 3){    
-
-            return x[0];
-            cout << x[0] << endl;
-        }
-        else
-            i++;
-            goto x;
-        case 42://*
-        cin >> x[i];
-        cin.ignore();
-        cin >> ch;
-        x[0]*=x[i];
-        if((int)ch == 3){    
-            
-            return x[0];
-            cout << x[0] << endl;
-        } 
-        else
-            i++;
-            goto x;
-        case 47:// /
-        cin >> x[i];
-        cin.ignore();
-        cin >> ch;
-        x[0]/=x[i];
-        if((int)ch == 3){    
-          
-            return x[0];
-            cout << x[0] << endl;
-        }
-        else
-            i++;
-            goto x;
-        }
 }
